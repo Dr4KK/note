18e0a9a376b5898ea94eeb5e279bd80f533b0ca0
kekai <kai.kedal@ele.me>
2015-12-03 21:11:13 +0800

    time to compute script running time

--- bash/tips.sh
+++ bash/tips.sh
@@ -3,5 +3,9 @@
 
 #设置程序运行时间
 #read 用于表示持续运行的进程
-TIMEOUT=100
+#time 命令用于统计程序执行时间
+TIMEOUT=10
 read & (sleep $TIMEOUT; eval 'kill -9 $!' &> /dev/null;)
+
+time (read & (sleep $TIMEOUT; eval 'kill -9 $!' &> /dev/null;))
+
